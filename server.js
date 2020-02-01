e58c3ea120923994ce8a91e890fe8c4591d6907a
Nicolás Jiménez <estnicjimsae@hotmail.com>
2020-02-01 16:16:41 -0500

    Taller de aplicaciones progresivas.

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const FORECAST_DELAY = 0;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = process.env.DARKSKY_API_KEY;
+const API_KEY = "328a47607baecf68522d3753796d7fcb";//process.env.DARKSKY_API_KEY;
+console.error(API_KEY);
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
